fd4b0979495f788d5fdba4f237a9f5f58fa6e6e7
Rafael Jeferson <rafa.jefer@gmail.com>
2019-06-08 21:16:00 -0300

    Corrigido bug no carousel

--- js/script.js
+++ js/script.js
@@ -1,37 +1,25 @@
-$(document).ready(function () {
 
-   // Exibi o primeiro item do carousel
-   $("#carousel .item-group").first().show();
-   
+var slideIndex = 0;
+showSlides();
 
-   // ao clicar no indicador do carousel
-   $(document).on('click', '.carousel-controls li', function () {
-      // Remove a classe de todos li controls
-      $('.carousel-controls li').removeClass('active');
-      // Adiciona a classe apenas no li control clicado
-      $(this).addClass('active');
-      // Pega o valor guardado na data-slide-to do item clicado
-      let slide_to = $(this).attr('data-slide-to');
-      // Oculta todos os sliders do carousel
-      $("#carousel .item-group").hide('fast');
-      // Mostra apenas o carousel que tiver com o indice igual o do data-slide-to clicado
-      $("#carousel .item-group:eq(" + slide_to + ")").show('slow');
-   });
-
-   
-   nextSlider();
-   function nextSlider() {
-      setInterval(function () {
-         let element = $('.carousel-controls li[class="active"]');
-         let next = parseInt(element.attr('data-slide-to')) + 1;
-         element.removeClass('active');
-         $(".carousel-controls li[data-slide-to= " + next + "]").addClass('active');
-         // Oculta todos os sliders do carousel
-         $("#carousel .item-group").hide('fast');
-         // Mostra apenas o carousel que tiver com o indice igual o do data-slide-to clicado
-         $("#carousel .item-group:eq(" + next + ")").show('slow');
-        
-      }, 3000);
-
-   }
+$(document).on('click', '.dot', function(){
+   slideIndex = ($(this).attr('data-slide-to'));
 });
+
+
+function showSlides() {
+   var slides = $(".item-group");
+   var dots = $(".dot");
+   
+   $.each(slides, function(){
+      slides.css('display', 'none');
+   });
+   slideIndex++;
+   if (slideIndex > slides.length) { slideIndex = 1; }
+   $.each(dots, function() {
+      dots.removeClass("active");
+   });
+   slides.eq(slideIndex-1).css('display', 'block');
+   dots.eq(slideIndex-1).addClass('active');
+   setTimeout(showSlides, 2000);
+}
